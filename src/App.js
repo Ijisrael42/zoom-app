b5faff7288844b5f655947efc5ef18ed0db3ee3c
Ijudah42 <ijisrael42@gmail.com>
2021-04-10 22:30:47 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useHistory } from 'react-router';
 import './App.css';
-// import React, { useEffect } from "react";
-// import { Link, Router } from "react-router-dom";
+import { config } from "./config";
+
 declare var ZoomMtg;
 
 ZoomMtg.setZoomJSLib('https://source.zoom.us/1.9.1/lib', '/av');
@@ -12,14 +12,14 @@ ZoomMtg.prepareWebSDK();
 function Zoom() {
 
   // setup your signature endpoint here: https://github.com/zoom/websdk-sample-signature-node.js
-  const signatureEndpoint = 'http://localhost:4000/questions/zoom-signature'
+  const signatureEndpoint = `${config.appUrl}/questions/zoom-signature`;
   const history = useHistory();
   // var meetingNumber = '74767890439'
   const role = 1
-  const leaveUrl = 'http://localhost:3000'
-  const userName = 'React'
-  const userEmail = 'ijisrael42@gmail.com'
-  const passWord = 'string'
+  const leaveUrl = config.zoomUrl;
+  const userName = 'React';
+  const userEmail = 'ijisrael42@gmail.com';
+  const passWord = 'string';
 
   // useEffect(() => {
     const getSignature = () => {
--- src/config.js
+++ src/config.js
@@ -2,5 +2,7 @@ export const config = {
   apiUrldev : "http://localhost:4000",
   apiUrl : "https://peaceful-crag-55555.herokuapp.com",
   appUrl : "https://test-f2acb.web.app",
-  appUrldev : "http://127.0.0.1:8100"
+  appUrldev : "http://127.0.0.1:8100",
+  zoomUrl: "https://dry-sierra-76380.herokuapp.com",
+  zoomUrldev: "http://localhost:3000"
 };
